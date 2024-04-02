f5d50046f8a8a5b1126c0de761915c491f219489
ssobii2 <subhanimran4@gmail.com>
2024-04-02 13:16:20 +0200

    Initial Commit

---
+++ locallibrary/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
